ddbe3572065f35b7b134f1216792e0acbc73a0d9
tymowskyy <tymowskyy@gmail.com>
2022-01-26 18:30:24 +0100

    added init.sql

---
+++ db/init.sql
@@ -0,0 +1,45 @@
+CREATE TABLE `lists` (
+  `list_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `tasks` (
+  `task_id` int(11) NOT NULL,
+  `list_id` int(11) NOT NULL,
+  `content` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+ALTER TABLE `lists`
+  ADD PRIMARY KEY (`list_id`),
+  ADD KEY `user_FK` (`user_id`);
+
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`task_id`),
+  ADD KEY `list_FK` (`list_id`);
+
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+ALTER TABLE `lists`
+  MODIFY `list_id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `tasks`
+  MODIFY `task_id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `lists`
+  ADD CONSTRAINT `user_FK` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `tasks`
+  ADD CONSTRAINT `list_FK` FOREIGN KEY (`list_id`) REFERENCES `lists` (`list_id`) ON DELETE CASCADE ON UPDATE CASCADE;
